b0c22f2750aed47cb2d440b239eb6bfbc943d70a
gewenyu99 <wenyu1999@hotmail.com>
2017-06-09 11:33:17 -0400

    Create esp8266_code.ino

--- esp8266_code/esp8266_code.ino
+++ esp8266_code/esp8266_code.ino
@@ -1,41 +1,38 @@
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-const char* ssid = "carnet-wifi";
-const char* password = "11111111";
+#include <ESP8266WiFi.h>//package for our specific wifi module
+#include <WiFiUdp.h>//package for wifi transmission
+const char* ssid = "carnet-wifi";//id for wireless server
+const char* password = "11111111";//pass for wireless server
 
-WiFiUDP Udp;
+WiFiUDP Udp;//instance of wireless UDP class that can requires 
 unsigned int localUdpPort = 42069;  // local port to listen on
 char incomingPacket[255];  // buffer for incoming packets
 
 void setup()
 {
+  //starts serial cycles
   Serial.begin(115200);
   Serial.println();
-
+  //connects to the wifi server setup
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.print(".");
+    Serial.print(".");//shows that there is a connection in serial monitor
   }
-  
-  Udp.begin(localUdpPort);
+  Udp.begin(localUdpPort);//begins the UDP for our wireless module to transfer information
 }
-
 
 void loop()
 {
-  int packetSize = Udp.parsePacket();
-  if (packetSize)
+  int packetSize = Udp.parsePacket();//reads packet
+  if (packetSize)//checks if such packet sizes exist
   {
-
-    int len = Udp.read(incomingPacket, 255);
-    if (len > 0)
+    int len = Udp.read(incomingPacket, 255);//length of packet stored
+    if (len > 0)//if packet is not empty, runs
     {
-      incomingPacket[len] = 0;
+      incomingPacket[len] = 0;//resets said variable 
     }
-    Serial.write(incomingPacket);
-
+    Serial.write(incomingPacket);//Sends contents through serial out to arduino
   }
 }
